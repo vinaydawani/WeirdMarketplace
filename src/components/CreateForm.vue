a291ee73eab21e9dfa68f712a25b4f47fc60b2ad
vinaydawani <vdawani6@gmail.com>
2021-06-11 14:37:42 -0400

    pins to ipfs

--- src/components/CreateForm.vue
+++ src/components/CreateForm.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    <form>
+    <form
+      id="NFTform"
+      enctype="multipart/form-data"
+      @submit.prevent="pinToIPFS"
+    >
       <div class="flex flex-col gap-y-8">
         <div class="flex flex-col">
           <label for="input-file" class="form-label"
             >Image, Video, or Audio *</label
           >
-          <span class="text-sm"
+          <span class="text-xs"
             >File types supported: JPG, PNG, GIF, SVG, MP4, WEBM, MP3, WAV, OGG,
             GLB, GLTF. Max size: 10 MB</span
           >
@@ -64,6 +68,7 @@
               </div>
               <input
                 type="file"
+                name="input-media-file"
                 @change="displayFile"
                 class="hidden cursor-pointer"
                 accept=".jpg, .png, .gif, .svg, .mp4, .webm, .mp3, .wav, .pdf"
@@ -74,21 +79,52 @@
         </div>
         <div class="flex flex-col">
           <label for="input-title" class="form-label">Title*</label>
-          <input type="text" id="input-title" class="form-input" required />
+          <input
+            type="text"
+            id="input-title"
+            name="input-title"
+            v-model="formData.title"
+            class="form-input"
+            required
+          />
         </div>
         <div class="flex flex-col">
           <label for="input-external-link" class="form-label"
             >External Link</label
           >
-          <input type="url" id="input-external-link" class="form-input" />
+          <input
+            type="url"
+            id="input-external-link"
+            name="input-external-link"
+            v-model="formData.url"
+            class="form-input"
+          />
         </div>
         <div class="flex flex-col">
           <label for="input-description" class="form-label">Description</label>
-          <textarea id="input-description" class="resize form-input" />
+          <textarea
+            id="input-description"
+            name="input-description"
+            v-model="formData.description"
+            class="resize form-input"
+          />
+        </div>
+        <div class="flex flex-col">
+          <label for="input-price" class="form-label">Fixed Price*</label>
+          <input
+            type="number"
+            step="0.0000001"
+            id="input-price"
+            name="input-price"
+            v-model="formData.price"
+            class="form-input"
+            required
+          />
         </div>
         <div class="flex items-center justify-center">
           <button
             type="submit"
+            id="NFTFormSubmitButton"
             class="w-40 h-12 border-gray-700 border-solid border-2 rounded-md"
           >
             Submit
@@ -100,13 +136,59 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
+  data() {
+    return {
+      formData: {
+        media: null,
+        title: "",
+        url: "",
+        description: "",
+        price: null,
+      },
+    };
+  },
   methods: {
     displayFile(e) {
-      const name = e.target.files[0].name;
-      console.log(name);
-      document.getElementById("file-name").innerHTML = name;
+      const size = e.target.files[0].size;
+      const limit = 10;
+      if (size / 10 ** 6 < limit) {
+        this.formData.media = e.target.files[0];
+        document.getElementById("file-name").innerHTML =
+          this.formData.media.name;
+      } else {
+        document.getElementById("file-name").innerHTML =
+          "File size greater than 10MB";
+      }
     },
+
+    pinToIPFS() {
+      // console.log(this.formData);
+      // const form = document.getElementById("NFTForm");
+      this.pinImage().then((data) => console.log(data.data));
+    },
+
+    async pinImage() {
+      let inputData = new FormData();
+      inputData.append("file", this.formData.media);
+      let x = await axios.post(
+        "https://api.pinata.cloud/pinning/pinFileToIPFS",
+        inputData,
+        {
+          maxBodyLength: "Infinity",
+          headers: {
+            "Content-Type": `multipart/form-data; boundary=${inputData._boundary}`,
+            pinata_api_key: "6ba02cfa7b5a88c13412",
+            pinata_secret_api_key:
+              "d32fa29c707ecb249b209a0a783ba2bc7bf165eca672a5b6cf4c7443c55ab955",
+          },
+        }
+      );
+      return x;
+    },
+    createMetadata(ipfsHash) {},
   },
 };
 </script>
